cf1766a05cfd9d044f69453d1f93d4d404a4b7d8
Jillberth Estillore <ejillberth@gmail.com>
2014-08-30 18:36:33 +0800

    Add base fragment class.

--- src/com/jillberthestillore/flames/fragments/BaseFragment.java
+++ src/com/jillberthestillore/flames/fragments/BaseFragment.java
@@ -2,10 +2,12 @@ package com.jillberthestillore.flames.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import butterknife.ButterKnife;
 
 import com.jillberthestillore.flames.BusProvider;
+import com.jillberthestillore.flames.R;
 import com.squareup.otto.Bus;
 
 public class BaseFragment extends Fragment {
@@ -34,6 +36,14 @@ public class BaseFragment extends Fragment {
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
 		ButterKnife.inject(this, view);
+	}
+	
+	protected void changeFragment(Fragment fragment) {
+		FragmentManager fragmentManager = getFragmentManager();
+		fragmentManager.beginTransaction()
+				.replace(R.id.container, fragment)
+				.addToBackStack(null)
+				.commit();
 	}
 	
 }
